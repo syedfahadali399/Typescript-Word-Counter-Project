c9e77274d69b9e22005ea1d084551772c4e2ff51
syed fahad ali <155745867+syedfahadali399@users.noreply.github.com>
2024-04-24 20:09:20 +0500

    Changes

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter Your Sentence To Count the Word"
+    }
+]);
+const words = answers.Sentence.trim().split("");
+console.log(words);
+console.log(`Your Sentence Word Count Is ${words.length}`);
---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt(
+    [
+        {
+            name: "Sentence",
+            type: "input",
+            message: "Enter Your Sentence To Count the Word"
+        }
+    ]
+)
+
+const words = answers.Sentence.trim().split("")
+
+console.log(words);
+
+console.log(`Your Sentence Word Count Is ${words.length}`);
